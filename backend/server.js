6e3bd6ae728ead0962d3d90c95e067e5d69c4f79
papry <jannatulferdouspapry115@gmail.com>
2025-12-05 21:38:56 +0600

    new update

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ import userRouter from './routes/userRoute.js';
 import productRouter from './routes/productRoute.js';
 import cartRouter from './routes/cartRoute.js';
 import orderRouter from './routes/orderRoute.js';
-import categoryRouter from './routes/CategoryRoute.js';
+import categoryRouter from './routes/categoryRoute.js';
 
 
 
